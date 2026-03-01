fbaf84e6a6a2c00f235156fa9f8199cbd1e8b701
Robin Houtkooper <21104964@student.hhs.nl>
2026-03-01 22:39:58 +0100

    Context update

--- DatabaseContext.cs
+++ DatabaseContext.cs
@@ -6,6 +6,9 @@ namespace FoodyBackend;
 
 public class DatabaseContext : DbContext
 {
+    public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options)
+    {
+    }
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         base.OnModelCreating(modelBuilder);
